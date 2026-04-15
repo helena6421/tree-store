0f6d485438aa90e00f18637d16237091978a4a75
helena6421 <40118858+helena6421@users.noreply.github.com>
2026-04-15 20:53:50 +0300

    chore: initialize project (Vite + Vue + TS)

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest/config" />
+
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+  },
+})
